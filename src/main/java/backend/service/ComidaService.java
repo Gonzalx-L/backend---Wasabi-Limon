c5b82d32b9eda55fe39c86272bb2893d92393bb9
Prueba <tony270604@gmail.com>
2025-06-20 19:42:24 -0500

    agrege comidaService

---
+++ src/main/java/backend/service/ComidaService.java
@@ -0,0 +1,15 @@
+package backend.service;
+
+import backend.dao.MozoRepository;
+import java.util.List;
+import backend.modelo.*;
+import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class ComidaService {
+    
+}
