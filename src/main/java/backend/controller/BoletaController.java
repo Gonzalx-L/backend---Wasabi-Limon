5ad43413bdfbfce573180516df40063e6538758c
Prueba <tony270604@gmail.com>
2025-07-01 01:57:16 -0500

    agrege paginacion a listar boletas y lo junte con el filtro para que sea solo un metodo

--- src/main/java/backend/controller/BoletaController.java
+++ src/main/java/backend/controller/BoletaController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 @RestController
 @RequestMapping("/api/boleta")
@@ -23,23 +26,22 @@ public class BoletaController {
     @Autowired
     private BoletaService boletaService;
 
-    @GetMapping("/listar")
-    public List<Map<String, Object>> findBoletas() {
-        return boletaService.findBoletas(null);
-    }
-
     //http://localhost:8080/api/boleta/filtrar?codMoz=0003&horaInicio=10:00:00&horaFin=20:00:00&total1=50&total2=180&fechaInicio=2025-05-20&fechaFin=2025-05-21
-    @GetMapping("/filtrar")
-    public List<Map<String, Object>> findBoletasFiltradas(
+    
+    @GetMapping("/listar")
+    public Page<Map<String, Object>> listarBoletasConFiltros(
             @RequestParam(required = false) String codMoz,
             @RequestParam(required = false) Float total1,
             @RequestParam(required = false) Float total2,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "HH:mm:ss") String horaInicio,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "HH:mm:ss") String horaFin,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") String fechaInicio,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") String fechaFin
+            @RequestParam(required = false) String horaInicio,
+            @RequestParam(required = false) String horaFin,
+            @RequestParam(required = false) String fechaInicio,
+            @RequestParam(required = false) String fechaFin,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int size
     ) {
-        return boletaService.findBoletasFiltradas(codMoz, total1, total2, horaInicio, horaFin, fechaInicio, fechaFin);
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return boletaService.findBoletasConFiltros(codMoz, total1, total2, horaInicio, horaFin, fechaInicio, fechaFin, pageable);
     }
 
     @GetMapping("/detallebol/{codBol}")
@@ -51,7 +53,7 @@ public class BoletaController {
     public List<Map<String, Object>> BoletaDetalleComidasCodigoOr(@PathVariable String codOr) {
         return boletaService.BoletaDetalleComidasCodigoOr(codOr);
     }
-    
+
     @GetMapping("/contarfecha")
     public int obtenerTotalPorFecha(@RequestParam("fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
         return boletaService.contarBoletasPorFecha(fecha);
